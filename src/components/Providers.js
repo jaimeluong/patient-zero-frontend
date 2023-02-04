4f9446cc9e48bfbc764d086147cbe0761fafe456
Jaime Luong <jaime.luong@gmail.com>
2023-02-04 00:34:04 -0600

    Add to Providers component

--- src/components/Providers.js
+++ src/components/Providers.js
@@ -1,13 +1,38 @@
+import React, { useState, useEffect } from 'react';
+
 const defaultStyle = {
     display: "flex",
     justifyContent: "center",
-    marginTop: "300px"
+    marginTop: "150px"
 };
 
+let url = "http://localhost:3000/providers";
+
 const Providers = () => {
+    const [providers, setProviders] = useState([]);
+
+    useEffect(() => {
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => {
+                setProviders(data);
+            })
+            .catch((error) => {
+                console.log(error.message);
+            });
+    }, []);
+
     return(
         <div className="Providers" style={defaultStyle}>
-            <h1>Providers content</h1>
+            {providers.map((provider) => {
+                return (
+                    <div className="provider" key={provider.id}>
+                        <p className="providerName">{provider.first_name} {provider.last_name}</p>
+                        <p className="providerName">Role: {provider.position}</p>
+                        <br />
+                    </div>
+                );
+            })}
         </div>
     );
 }
