d5606f89e0d139cbc379b14ba013d2a0bbda5c9f
Jaime Luong <jaime.luong@gmail.com>
2023-02-04 00:27:37 -0600

    Add to Patients component

--- src/components/Patients.js
+++ src/components/Patients.js
@@ -1,13 +1,39 @@
+import React, { useState, useEffect } from 'react';
+
 const defaultStyle = {
     display: "flex",
     justifyContent: "center",
-    marginTop: "300px"
+    marginTop: "150px"
 };
 
+let url = "http://localhost:3000/patients";
+
 const Patients = () => {
+    const [patients, setPatients] = useState([]);
+
+    useEffect(() => {
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => {
+                setPatients(data);
+            })
+            .catch((error) => {
+                console.log(error.message);
+            });
+    }, []);
+
     return(
         <div className="Patients" style={defaultStyle}>
-            <h1>Patients content</h1>
+            {patients.map((patient) => {
+                return (
+                    <div className="patient" key={patient.id}>
+                        <p className="patientName"><strong>{patient.first_name} {patient.last_name}</strong></p>
+                        <p className="patientInfo">Age: {patient.age}, Sex: {patient.sex}</p>
+                        <p className="medicalNotes">Medical notes: {patient.medical_notes}</p>
+                        <br />
+                    </div>
+                );
+            })}
         </div>
     );
 }
