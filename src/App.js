b76a81e88cd776332c882d261697a66c708abb5f
Jaime Luong <jaime.luong@gmail.com>
2023-02-03 23:16:04 -0600

    Install React router, add page title, create components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
-import Navbar from './components/Navbar.js';
-import Footer from './components/Footer.js';
+// import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './components/Layout.js';
+import Home from './components/Home.js';
+import Providers from './components/Providers.js';
+import Patients from './components/Patients.js';
+import Appointments from './components/Appointments.js';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="providers" element={<Providers />} />
+            <Route path="patients" element={<Patients />} />
+            <Route path="appointments" element={<Appointments />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
