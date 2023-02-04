99391810ed2a66750badf6674e7381b68d79e09c
Jaime Luong <jaime.luong@gmail.com>
2023-02-04 00:47:10 -0600

    Add to Appointments component

--- src/components/Appointments.js
+++ src/components/Appointments.js
@@ -1,13 +1,44 @@
-const defaultStyle = {
-    display: "flex",
-    justifyContent: "center",
-    marginTop: "300px"
-};
+import React, { useState, useEffect } from 'react';
+
+const center = {
+    textAlign: "center"
+}
+
+const top = {
+    marginTop: "150px",
+    marginBottom: "-300px"
+}
+
+let url = "http://localhost:3000/appointments";
 
 const Appointments = () => {
+    const [appointments, setAppointments] = useState([]);
+
+    useEffect(() => {
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => {
+                setAppointments(data);
+            })
+            .catch((error) => {
+                console.log(error.message);
+            });
+    }, []);
+
     return(
-        <div className="Appointments" style={defaultStyle}>
-            <h1>Appointments content</h1>
+        <div className="Appointments" style={top}>
+            <h1 style={center}>View appointments</h1>
+            <br />
+            {appointments.map((appointment) => {
+                return (
+                    <div className="appointments" key={appointments.id}>
+                        <p className="dates"><strong>Time: </strong>{appointment.start_date} to {appointment.start_date}</p>
+                        <p className="category"><strong>Category: </strong>{appointment.category}</p>
+                        <p className="description"><strong>Description: </strong>{appointment.description}</p>
+                        <br />
+                    </div>
+                );
+            })}
         </div>
     );
 }
